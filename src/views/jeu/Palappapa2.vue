3c4dc5fee4945c900cafc922052bc2ce6cccc925
spiderLambo <spiderLambo.dev@outlook.com>
2026-05-05 01:15:53 +0200

    [+]>Scene 2

--- src/views/jeu/Palappapa2.vue
+++ src/views/jeu/Palappapa2.vue
@@ -1,3 +1,55 @@
-<script setup></script>
+<script setup>
+import Poppy from "../Poppy.vue";
+</script>
 
-<template><h1>coucou</h1></template>
+<template>
+  <section>
+    <img src="../../assets/Jeu/palappapa/2/fg.png" id="fg" />
+    <img src="../../assets/Jeu/palappapa/2/fleur.svg" id="fleur" />
+    <Poppy
+      :spawn="[2, 0]"
+      :tp="{
+        gauche: {
+          positions: [],
+          link: ``,
+        },
+        droite: {
+          positions: [],
+          link: ``,
+        },
+        haut: {
+          positions: [],
+          link: ``,
+        },
+        bas: {
+          positions: [],
+          link: ``,
+        },
+      }"
+      :interdis="[]"
+    />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  background: no-repeat url("../../assets/Jeu/palappapa/2/bg.png") center/cover;
+  height: 100dvh;
+  width: 100dvw;
+  display: flex;
+
+  #fg {
+    height: 100dvh;
+    aspect-ratio: 758 / 1080;
+    z-index: 100;
+  }
+
+  #fleur {
+    position: absolute;
+    height: 8.3vh;
+    left: 50vw;
+    top: 50vh;
+    aspect-ratio: 812 / 354;
+  }
+}
+</style>
